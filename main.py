51677605d38b6bcc98007e58230debc993e0154d
hikinukk <55659899+hikinukk@users.noreply.github.com>
2021-07-15 02:32:28 +0900

    画像のフィルタ効果、反転を実装 右クリックポップアップメニューにグレースケール、色反転、左右反転上下反転を追加した。

--- main.py
+++ main.py
@@ -20,6 +20,10 @@ class viewerGUI(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
 
+        self.color_filter = "default"
+        self.is_image_flip_horizontal = False
+        self.is_image_flip_upside_down =False
+
         self.create_canvas()
         self.create_popupmenu()
         self.create_key_handler()
@@ -45,7 +49,19 @@ class viewerGUI(tk.Frame):
         # 配列に変換
         self.cv_img = np.array(self.grab_image)
         # 色変換
-        self.cv_img= cv2.cvtColor(self.cv_img, cv2.COLOR_RGB2BGR)
+        if self.color_filter == "default":
+            self.cv_img= cv2.cvtColor(self.cv_img, cv2.COLOR_RGB2BGR)
+        elif self.color_filter == "gray":
+            self.cv_img= cv2.cvtColor(self.cv_img, cv2.COLOR_BGR2GRAY)
+        elif self.color_filter == "inversion":
+            self.cv_img= cv2.cvtColor(self.cv_img, cv2.COLOR_RGB2BGR)
+            self.cv_img = cv2.bitwise_not(self.cv_img)
+        # 画像編集
+        if self.is_image_flip_horizontal:
+            self.cv_img = cv2.flip(self.cv_img, 1)
+        if self.is_image_flip_upside_down:
+            self.cv_img = cv2.flip(self.cv_img, 0)
+
         # canvasに画像を表示
         self.im = ImageTk.PhotoImage(image=Image.fromarray(self.cv_img))
         self.canvas.create_image(0, 0, image=self.im, anchor='nw')
@@ -67,20 +83,50 @@ class viewerGUI(tk.Frame):
 
     def create_popupmenu(self):
         # ポップアップメニューの設定
-        self.popUpMenu = tk.Menu(
-            root,
-            tearoff = False, # tearoff=False:指定しないとメニューがきりとられちゃう
-        )
-        # [ポップアップメニュー] - [Command-1]
-        self.popUpMenu.add_command(
-            label = "終了",
-            command=self.command_1
-        )
+        self.popUpMenu = tk.Menu(root, tearoff = False)
+        self.popUpMenu_filter = tk.Menu(self.popUpMenu, tearoff = False)
+        self.popUpMenu_flip = tk.Menu(self.popUpMenu, tearoff = False)
+
+        # [ポップアップメニュー] - [Command]
+        self.popUpMenu.add_cascade(label='フィルタ',menu=self.popUpMenu_filter,under=5)
+        self.popUpMenu_filter.add_command(label='デフォルト',underline=5,command=self.command_filter_default)
+        self.popUpMenu_filter.add_command(label='グレースケール',underline=5,command=self.command_filter_gray)
+        self.popUpMenu_filter.add_command(label='色反転',underline=5,command=self.command_filter_color_inversion)
+
+        self.popUpMenu.add_cascade(label='画面表示',menu=self.popUpMenu_flip,under=5)
+        self.popUpMenu_flip.add_command(label='デフォルト',underline=5,command=self.command_flip_default)
+        self.popUpMenu_flip.add_command(label='左右反転',underline=5,command=self.command_flip_horizontal)
+        self.popUpMenu_flip.add_command(label='上下反転',underline=5,command=self.command_flip_upside_down)
+
+        self.popUpMenu.add_command(label = "終了", command=self.command_finish)
+
+
         # 右クリックイベント関連付け
         root.bind("<Button-3>", self.show_popupmenu)
 
-    # Command1処理
-    def command_1(self):
+    # Command処理
+    def command_filter_default(self): # bデフォルト
+        self.color_filter = "default"
+    def command_filter_gray(self): # グレースケール
+        self.color_filter = "gray"
+    def command_filter_color_inversion(self): # 色反転
+        self.color_filter = "inversion"
+
+    def command_flip_default(self): # デフォルト
+        self.is_image_flip_horizontal = False
+        self.is_image_flip_upside_down = False
+    def command_flip_horizontal(self): # 左右反転
+        if not self.is_image_flip_horizontal:
+            self.is_image_flip_horizontal = True
+        else:
+            self.is_image_flip_horizontal = False
+    def command_flip_upside_down(self): # 上下反転
+        if not self.is_image_flip_upside_down:
+            self.is_image_flip_upside_down = True
+        else:
+            self.is_image_flip_upside_down = False
+
+    def command_finish(self):
         print("終了")
         sys.exit()
 
