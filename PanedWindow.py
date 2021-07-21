792ada7d5fdb3512484a0cbb57143c315750f9ad
hikinukk <55659899+hikinukk@users.noreply.github.com>
2021-07-21 18:59:49 +0900

    ２個目以降のウィンドウも分割できるように 新規ウィンドウ作ってpanedwindowに追加する処理を実装。大きさを指定してないので追加されてるかちょっと見にくい。

--- PanedWindow.py
+++ PanedWindow.py
@@ -24,51 +24,39 @@ class panedWindow(tk.Frame):
     def __init__(self, root ,master=None ):
         super().__init__(master)
 
-        self.paned_window_list = []
+        # self.paned_window_list = []
 
         # PanedWindowの作成
-        self.paned_window = tk.PanedWindow(root, sashwidth = 4)
+        paned_window = tk.PanedWindow(root, sashwidth = 4)
+        paned_window.pack(expand = True, fill = tk.BOTH)
 
         # Frameの作成
-        self.widget1 = imgFrame.frameGUI(root)
+        widget1 = imgFrame.frameGUI(root)
+        widget1.popUpMenu.add_command(label='ウィンドウ分割',underline=5,command=lambda:self.create_paned_frame(paned_window, root))
+        widget1.update()
 
         # フレームをPanedWindowに追加
-        self.paned_window.add(self.widget1.app_frame)
+        paned_window.add(widget1.app_frame)
 
-        self.paned_window.pack(expand = True, fill = tk.BOTH)
+        # self.paned_window_list.append(self.paned_window)
 
-        # 右クリックメニュー追加
-        self.widget1.popUpMenu.add_command(label='ウィンドウ分割',underline=5,command=lambda:self.create_paned_frame(root))
-
-        self.paned_window_list.append(self.paned_window)
-
-        self.widget1.update()
-
-        # self.mainloop()
-    def create_paned_frame(self, root):
-        print("root")
+    def create_paned_frame(self, parentWindow, root):
         # PanedWindowの作成
-        paned_window2 = tk.PanedWindow(self.paned_window, sashwidth = 4)
-        
-        # Frameの作成
-        # self.widget1 = self.widget1
-        self.widget2 = imgFrame.frameGUI(root)
-
-        # 右クリックメニュー追加
-        self.widget2.popUpMenu.add_command(label='ウィンドウ分割',underline=5,command=lambda:self.create_paned_frame(root))
-        
-        # フレームをPanedWindowに追加
-        paned_window2.add(self.widget1.app_frame)
-        paned_window2.add(self.widget2.app_frame)
-        self.widget2.update()
-
-        # 親PanedWindowにPanedWindowを追加
-        self.paned_window.add(paned_window2)
-
+        paned_window2 = tk.PanedWindow(parentWindow, sashwidth = 4)
         paned_window2.pack(expand = True, fill = tk.BOTH)
+        
+        # Frameの作成
+        widget2 = imgFrame.frameGUI(root)
+        widget2.popUpMenu.add_command(label='ウィンドウ分割',underline=5,command=lambda:self.create_paned_frame(paned_window2, root))
+        widget2.update()
+        
+        # フレームをPanedWindowに追加
+        paned_window2.add(parentWindow.panes()[0]) # クリック元のFrame
+        paned_window2.add(widget2.app_frame) # 新しいFrame
+        
+        # 親PanedWindowにPanedWindowを追加
+        parentWindow.add(paned_window2)
 
         # self.paned_window_list.append(paned_window2)
-        self.widget2.update()
 
-        self.paned_window.pack(expand = True, fill = tk.BOTH)
 
